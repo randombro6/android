d64595dc815a7149cc3413e1555fc8f277487b3d
randombro6 <148932897+randombro6@users.noreply.github.com>
2024-01-06 03:05:00 +0530

    Update l5.java

--- l5.java
+++ l5.java
@@ -1,28 +1,27 @@
-package com.example.calc;
+package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-//THE APP WILL WORK ONLY FOR SINGLE OPERATION AT A TIME, I.E, DO 2+3 NOT 2+3+3
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     Button one, two, three, four, five, six, seven, eight, nine;
     Button add, sub, div, mul, clear, equals;
     EditText res;
     String operatorPressed = " ";
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        res= findViewById(R.id.res); //EditText
+        res = findViewById(R.id.res); //EditText
         one = findViewById(R.id.one);
         two = findViewById(R.id.two);
         three = findViewById(R.id.three);
@@ -39,8 +38,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         div = findViewById(R.id.div);
         equals = findViewById(R.id.equals);
 
-
-
         one.setOnClickListener(this);
         two.setOnClickListener(this);
         three.setOnClickListener(this);
@@ -50,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         seven.setOnClickListener(this);
         eight.setOnClickListener(this);
         nine.setOnClickListener(this);
-// operators
+        // operators
         add.setOnClickListener(this);
         sub.setOnClickListener(this);
         mul.setOnClickListener(this);
@@ -59,70 +56,83 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         clear.setOnClickListener(this);
     }
 
-    
     @Override
     public void onClick(View view) {
         double finalResult = 0.0;
-        switch(view.getId())
-        {
-            case R.id.one: res.append("1");
+        switch (view.getId()) {
+            case R.id.one:
+                res.append("1");
                 break;
-            case R.id.two: res.append("2");
+            case R.id.two:
+                res.append("2");
                 break;
-            case R.id.three: res.append("3");
+            case R.id.three:
+                res.append("3");
                 break;
-            case R.id.four: res.append("4");
+            case R.id.four:
+                res.append("4");
                 break;
-            case R.id.five: res.append("5");
+            case R.id.five:
+                res.append("5");
                 break;
-            case R.id.six: res.append("6");
+            case R.id.six:
+                res.append("6");
                 break;
-            case R.id.seven: res.append("7");
+            case R.id.seven:
+                res.append("7");
                 break;
-            case R.id.eight: res.append("8");
+            case R.id.eight:
+                res.append("8");
                 break;
-            case R.id.nine: res.append("9");
+            case R.id.nine:
+                res.append("9");
                 break;
-
-            case R.id.add: res.append("+");
-                operatorPressed="+";
+            case R.id.add:
+                res.append("+");
+                operatorPressed = "+";
                 break;
-            case R.id.sub: res.append("-");
-                operatorPressed="-";
+            case R.id.sub:
+                res.append("-");
+                operatorPressed = "-";
                 break;
-            case R.id.mul: res.append("*");
-                operatorPressed="*";
+            case R.id.mul:
+                res.append("*");
+                operatorPressed = "*";
                 break;
-            case R.id.div: res.append("/");
-                operatorPressed="/";
+            case R.id.div:
+                res.append("/");
+                operatorPressed = "/";
                 break;
-            case R.id.clear: res.setText(" ");
+            case R.id.clear:
+                res.setText(" ");
                 break;
-            case R.id.equals: finalResult=
-                    evaluateExpression(res.getText().toString(),operatorPressed);
-                    res.setText(String.valueOf(finalResult));
-                    break;
-
-            default:return;
+            case R.id.equals:
+                finalResult =
+                        evaluateExpression(res.getText().toString(), operatorPressed);
+                res.setText(Double.toString(finalResult));
+                break;
+            default:
+                return;
         }
-
-
-
-
     }
-    private double evaluateExpression(String res, String operatorPressed)
-    {
+
+    private double evaluateExpression(String res, String operatorPressed) {
         String[] tokens = res.split("\\+|-|\\*|\\/");
 
         double firstOperand = Double.parseDouble(tokens[0]);
         double secondOperand = Double.parseDouble(tokens[1]);
-        switch(operatorPressed)
-        {
-            case "+": return firstOperand + secondOperand;
-            case "-": return firstOperand - secondOperand;
-            case "*": return firstOperand * secondOperand;
-            case "/": return firstOperand / secondOperand;
-            default: return 0;
+        switch (operatorPressed) {
+            case "+":
+                return firstOperand + secondOperand;
+            case "-":
+                return firstOperand - secondOperand;
+            case "*":
+                return firstOperand * secondOperand;
+            case "/":
+                return firstOperand / secondOperand;
+            default:
+                return 0;
         }
     }
+
 }

1df102c65e43b7eb2dd7fe0a7de4b109dcaef9e2
randombro6 <148932897+randombro6@users.noreply.github.com>
2024-01-07 13:05:42 +0530

    Update l2.java

--- l2.java
+++ l2.java
@@ -2,15 +2,15 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle ob){
         super.onCreate(ob);
         setContentView(R.layout.activity_main);
-        
+
         WebView wv = findViewById(R.id.web);
-        wv.loadUrl("url");
+        wv.loadUrl("https://www.facebook.com");
+
 
         ListView lv = findViewById(R.id.list);
-        String countryList [] = getResources().getStringArray(R.array.country);
-
-        ArrayAdapter<String> adap = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, countryList);
-        lv.setAdapter(adap);
+        String cList [] = getResources().getStringArray(R.array.countryList);
+        ArrayAdapter<String> ad = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, cList);
+        lv.setAdapter(ad);
 
     }
 }
